ff35cd8dfbeb2d29b92a6732ddbf6d60a7fdbb35
KevinCadena <65985084+stayclassyinfo@users.noreply.github.com>
2021-01-21 16:38:01 -0500

    updated code

--- js/index.js
+++ js/index.js
@@ -5,19 +5,33 @@ var section3 = document.getElementById("section3");
 
 var timesPerSecond = 15; // how many times to fire the event per second
 var wait = false;
+
 var updateHeight = function(event) {
-    
+    var percentage = Math.floor(event.clientY / window.innerHeight * 100);
+
+
     if (!wait) {
-    if((event.clientY / 100) <= 6 && (event.clientY / 100) >= 1 ) { 
+    if(percentage <= 60 && percentage >= 10) { 
         section1.style.height = event.clientY + 'px'; 
-    }
-
-    if(((section1.offsetHeight + section3.offsetHeight - 10) / 100) <= 9.1 && ((section1.offsetHeight + section3.offsetHeight - 10) / 100) >= 1.2 ) { 
         section2.style.height = "calc(100% - " + (section1.offsetHeight + section3.offsetHeight) + 'px'; 
+
+    }
+    if(percentage >= 70) {
+        section2.style.height = event.clientY - section1.offsetHeight + 'px'; 
+        section3.style.height = "calc(100% - " + (section1.offsetHeight + section2.offsetHeight) + 'px'; 
     }
 
-    if((event.clientY / 100) <= 9.1 && (event.clientY / 100) >= 6.8 ) {   section3.style.height = "calc(100% - " + event.clientY + 'px';
-    }
+
+
+    // if(percentage > divHeightThree) {
+    //     section3.style.height = "calc(100% - " + event.clientY + 'px';
+    //     section2.style.height = "calc(100% - " + (section1.offsetHeight + section3.offsetHeight) + 'px'; 
+
+    // }
+    // if(((section1.offsetHeight + section3.offsetHeight - 10) / 100) <= 9.1 && ((section1.offsetHeight + section3.offsetHeight - 10) / 100) >= 1.2 ) { 
+    // }
+
+
 
     wait = true; 
     background.removeEventListener('mousemove', updateHeight);
